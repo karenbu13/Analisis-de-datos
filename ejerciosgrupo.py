926d65566cb9bde875da22df1007a513811a9610
karenbu13 <karenbu13@gmail.com>
2025-09-17 02:43:19 +0000

    Descripción de los cambios

---
+++ ejerciosgrupo.py
@@ -0,0 +1,10 @@
+
+#solicita al usuario dos cadenas comparalas y verifica si son o no son iguales
+cadena1 = input("ingrese la primera cadena: ")
+cadena2 = input("ingrese la segunda cadena: ")
+if cadena1 == cadena2:
+    print("las cadenas son iguales") 
+else:   
+    print("las cadenas no son iguales")   
+          
+
